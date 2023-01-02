3f270e666a59a50d59fbc65b9cefb4ab0e4cd43f
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2023-01-02 23:31:38 +0000

    Bump compute module

--- solutions/virtual-desktop/avd.pooled.vm.tf
+++ solutions/virtual-desktop/avd.pooled.vm.tf
@@ -5,7 +5,7 @@
 module "avdpo" {
   # source = "github.com/DevSecNinja/terraform-azurerm-compute?ref=main"
   source  = "DevSecNinja/compute/azurerm"
-  version = "1.1.3"
+  version = "1.1.4"
 
   ### Important
   instances = 1
